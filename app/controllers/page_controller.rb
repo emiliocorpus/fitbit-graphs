178a8511ebabebaa4e70e8df8dbb3fcff0aa6ae4
emiliocorpus <emiliocorpus@gmail.com>
2016-04-03 16:27:44 -0700

    adds user controller

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,11 @@
 class PageController < ApplicationController
+
+	# home page action
   def home
  
+  end
+
+  def user
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
 
+  get 'user/index'
+
+  get 'user/new'
+
+  get 'user/login'
+
+  get 'user/logout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
   # You can have the root of your site routed with "root"
   root 'page#home'
+
+  get 'users/:id' => 'page#user'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
