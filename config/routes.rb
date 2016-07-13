40e7951db25e877e566f7232f5d8ab6ccc86b03b
emiliocorpus <emiliocorpus@gmail.com>
2016-07-13 16:00:04 -0700

    reverts to devise methods

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,4 @@ Rails.application.routes.draw do
   get 'demo/' => 'page#demo'
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
 
-  delete 'users/sign_out' => 'page#sign_out', as: 'sign_out'
-
 end
