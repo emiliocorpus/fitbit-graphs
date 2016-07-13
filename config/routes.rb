58ceed2f15ea4cdd99d17895654177b4e9cec99e
emiliocorpus <emiliocorpus@gmail.com>
2016-07-13 15:43:01 -0700

    adds resources at end of routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
+  
   # You can have the root of your site routed with "root"
   root 'page#home'
 
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   get 'fitbit/:resource/:date.json' => 'fitbit_api#data_request', as: 'fitbit_api_request'
 
   get 'demo/' => 'page#demo'
-  
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
 end
